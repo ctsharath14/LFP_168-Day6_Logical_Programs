cc32e307e508ae8ed02e1fcdc0911713a8384801
C T SOMAIAH <102033298+ctsharath14@users.noreply.github.com>
2022-07-30 16:41:29 +0530

    Update PrimeNumber.java

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -1,25 +1,17 @@
 import java.util.Scanner;
 
 public class PrimeNumber {
-
 	public static void main(String[] args) {		
-		int num;
+		int count = 0;
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter Number: ");
-		num = sc.nextInt();
-		CheckPrimeNums(num);
-
-	}
-
-	private static void CheckPrimeNums(int n) {
-		
-		int count = 1;
+		int num = sc.nextInt();
 		for(int i = 2; i <= n; i++) {
 			if(n % i == 0)
-				count++;
+			count++;
 		}
 		
-		if(count == 2)
+		if(count == 1)
 			System.out.println(n + " is a prime number.");
 		else
 			System.out.println(n + " is not a prime number.");
